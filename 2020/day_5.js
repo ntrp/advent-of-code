562c05072878a29ade4e6804f292a06a7423f9cf
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2020-12-07 22:10:18 +0100

    day 5

---
+++ 2020/day_5.js
@@ -0,0 +1,35 @@
+data = document
+  .querySelector("body>pre")
+  .innerText.split("\n")
+  .filter((str) => str.length > 0)
+  .map((str) => ({
+    row: str
+      .substr(0, 7)
+      .split("")
+      .reduce(
+        (prev, curr) =>
+          curr === "F"
+            ? prev.slice(0, prev.length / 2)
+            : prev.slice(prev.length / 2, prev.length),
+        [...Array(128).keys()]
+      )[0],
+    col: str
+      .substr(7, 3)
+      .split("")
+      .reduce(
+        (prev, curr) =>
+          curr === "L"
+            ? prev.slice(0, prev.length / 2)
+            : prev.slice(prev.length / 2, prev.length),
+        [...Array(8).keys()]
+      )[0],
+  }))
+  .map((data) => data.row * 8 + data.col)
+  .sort((a, b) => a - b);
+
+// P1
+console.log(`Part1: ${data[data.length - 1]}`);
+// P2
+console.log(
+  `Part2: ${data.find((num, idx, arr) => arr[idx + 1] !== num + 1) + 1}`
+);
