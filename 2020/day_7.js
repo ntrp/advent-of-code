a87c68aa67c458bfcf670039219d15361a30172c
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2020-12-08 13:06:20 +0100

    day 7

---
+++ 2020/day_7.js
@@ -0,0 +1,50 @@
+data = document
+  .querySelector("body>pre")
+  .innerText.split("\n")
+  .filter((str) => str.length > 0)
+  .map((str) => ({
+    .../^(?<bag>[a-z]+ [a-z]+) bags contain(?<rest>( (\d+ \w+ \w+|no other) bags?,?)+)/g.exec(
+      str
+    ).groups,
+  }))
+  .reduce((prev, curr) => ({ ...prev, [curr.bag]: curr.rest }), {});
+
+computeParents = (bagName) => {
+  parents = Object.entries(data)
+    .filter(([, rest]) => rest.indexOf(bagName) > -1)
+    .map(([bag]) => bag);
+  if (parents.length > 0) {
+    return new Set([
+      ...parents,
+      ...parents
+        .map((parent) => computeParents(parent))
+        .reduce((prev, curr) => [...prev, ...curr], []),
+    ]);
+  }
+  return [];
+};
+
+computeAllBags = (bagName) => {
+  return Object.entries(
+    data[bagName]
+      .split(",")
+      .map((str) => ({
+        .../(?<weight>\d+) (?<destination>\w+ \w+)/.exec(str)?.groups,
+      }))
+      .filter((obj) => !!obj.destination)
+      .reduce(
+        (prev, curr) => ({
+          ...prev,
+          [curr.destination]: parseInt(curr.weight),
+        }),
+        {}
+      )
+  )
+    .map(([bag, num]) => num * computeAllBags(bag))
+    .reduce((a, b) => a + b, 1);
+};
+
+// P1
+console.log(`Part1: ${computeParents("shiny gold").size}`);
+// P2
+console.log(`Part2: ${computeAllBags("shiny gold") - 1}`);
