30398e1c439aaa874d5993a6650344c53f4eb3fe
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2021-12-07 20:06:08 +0100

    WIP

--- 2021/solution/haskell/days/Day07.hs
+++ 2021/solution/haskell/days/Day07.hs
@@ -1,23 +1,42 @@
-module Main(main) where
+module Main (main) where
 
 import Common
 
 -- $setup
--- >>> let testInput = "te\nst"
+-- >>> let testInput = "16,1,2,0,4,2,7,1,2,14"
+
+-- | parse a comma separated int list
+--
+-- >>> parse' dataParser testInput
+-- Right [16,1,2,0,4,2,7,1,2,14]
+dataParser :: Parser [Int]
+dataParser = map read <$> sepBy (many1 digit) (string ",")
+
+-- | bruteforce fuel solving
+--
+-- >>> solveFuel $ fromRight [] $ parse' dataParser testInput
+-- 37
+solveFuel :: (Int -> Int -> Int) -> [Int] -> Int
+solveFuel distanceFn xs = minimum $ map (\pos -> sum $ map (distanceFn pos) xs) [minimum xs .. maximum xs]
 
 -- | should output the correct value give in the test input
 --
 -- >>> part1 $ lines testInput
--- "not implemented"
+-- "37"
 part1 :: [String] -> String
-part1 = notImplemented
+part1 = show . solveFuel . fromRight [] . parse' dataParser . unlines
+
+tick :: 
+
+solveFuelInc :: [Int] -> Int
+solveFuelInc xs = 0
 
 -- | should output the correct value give in the test input
 --
 -- >>> part2 $ lines testInput
--- "not implemented"
+-- "168"
 part2 :: [String] -> String
-part2 = notImplemented
+part2 = show . solveFuelInc . fromRight [] . parse' dataParser . unlines
 
 main :: IO ()
 main = interact $ solution part1 part2
