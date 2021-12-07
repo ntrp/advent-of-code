83ae0c95a166a940e92928e84a29b5e96fab072e
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2021-12-07 18:10:53 +0100

    fails..

--- 2021/solution/haskell/days/Day07.hs
+++ 2021/solution/haskell/days/Day07.hs
@@ -1,16 +1,36 @@
-module Main(main) where
+module Main (main) where
 
 import Common
+import Data.List (elemIndex, findIndex)
+import Data.Maybe
 
 -- $setup
--- >>> let testInput = "te\nst"
+-- >>> let testInput = "16,1,2,0,4,2,7,1,2,14"
+
+-- | parse a comma separated int list
+--
+-- >>> parse' dataParser testInput
+-- Right [16,1,2,0,4,2,7,1,2,14]
+dataParser :: Parser [Int]
+dataParser = map read <$> sepBy (many1 digit) (string ",")
+
+-- | bruteforce fuel solving
+--
+-- >>> solveFuel $ fromRight [] $ parse' dataParser testInput
+-- 2
+solveFuel :: [Int] -> Int
+solveFuel xs = 
+  let fuels = map (\pos -> sum $ map (\c -> abs (pos - c)) xs) [minimum xs .. maximum xs]
+   in fromMaybe (-1) $ elemIndex (minimum fuels) fuels
+   --in fuels
+   --in [minimum xs, maximum xs]
 
 -- | should output the correct value give in the test input
 --
 -- >>> part1 $ lines testInput
--- "not implemented"
+-- "2"
 part1 :: [String] -> String
-part1 = notImplemented
+part1 = show . solveFuel . fromRight [] . parse' dataParser . unlines
 
 -- | should output the correct value give in the test input
 --
