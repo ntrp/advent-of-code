cc18ef4550d78f6ed25990dce39c87a9f972f16f
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2022-12-04 15:25:17 +0100

    day2 done

---
+++ 2022/solution/rust/src/days/day02.rs
@@ -0,0 +1,67 @@
+use nom::{
+    character::complete::{newline, one_of, space1},
+    combinator::map,
+    error::ErrorKind,
+    multi::separated_list1,
+    sequence::tuple,
+};
+
+use crate::problem::{self, Solution};
+
+pub struct Day02;
+
+impl Solution for Day02 {
+    fn name(&self) -> &'static str {
+        "Rock Paper Scissors"
+    }
+
+    fn part_a(&self, test: bool) -> String {
+        let rounds = load(test);
+        rounds.iter().map(|(him, me)| compute_score(*him, *me)).sum::<usize>().to_string()
+    }
+
+    fn part_b(&self, test: bool) -> String {
+        let rounds = load(test);
+        rounds.iter().map(|(him, me)| compute_score(*him, prepare(*him, *me))).sum::<usize>().to_string()
+    }
+}
+
+const HIM: &str = "ABC";
+const ME: &str = "XYZ";
+
+fn load(test: bool) -> Vec<(usize, usize)> {
+    let data = problem::load(2, test);
+    let round = map(
+        tuple((
+            one_of::<_, _, (&str, ErrorKind)>(HIM),
+            space1,
+            one_of::<_, _, (&str, ErrorKind)>(ME),
+        )),
+        |(him, _, me)| (HIM.find(him).unwrap(), ME.find(me).unwrap()),
+    );
+    let mut rounds = separated_list1(newline, round);
+    match rounds(data.as_str()) {
+        Ok((_, res)) => res,
+        Err(_) => todo!(),
+    }
+}
+
+fn prepare(him: usize, me: usize) -> usize {
+    if me == 0 {
+        (him as i32 - 1).rem_euclid(3) as usize
+    } else if me == 1 {
+        him
+    } else {
+        (him + 1).rem_euclid(3)
+    }
+}
+
+fn compute_score(him: usize, me: usize) -> usize {
+    let mut score = me + 1;
+    if him == me {
+        score += 3
+    } else if me == ((him + 1) % 3) {
+        score += 6
+    }
+    score
+}
--- 2022/solution/rust/src/days/mod.rs
+++ 2022/solution/rust/src/days/mod.rs
@@ -1,5 +1,6 @@
 use crate::problem::Solution;
 
 mod day01;
+mod day02;
 
-pub const ALL: [&dyn Solution; 1] = [&day01::Day01];
+pub const ALL: [&dyn Solution; 2] = [&day01::Day01, &day02::Day02];
