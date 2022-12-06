562051704fcba2c9b46e1816b4fc181c7d154ef3
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2022-12-06 08:54:43 +0100

    day6 done

---
+++ 2022/solution/rust/src/days/day06.rs
@@ -0,0 +1,58 @@
+use std::{collections::HashSet, usize};
+
+use crate::problem::{self, Solution};
+
+pub struct Day06;
+
+impl Solution for Day06 {
+    fn name(&self) -> &'static str {
+        "Tuning Trouble"
+    }
+
+    fn part_a(&self, test: bool) -> String {
+        let stream = load(test);
+        find_marker(stream, 4).to_string()
+    }
+
+    fn part_b(&self, test: bool) -> String {
+        let stream = load(test);
+        find_marker(stream, 14).to_string()
+    }
+}
+
+fn load(test: bool) -> String {
+    problem::load(6, test)
+}
+
+fn find_marker(stream: String, size: usize) -> usize {
+    let mut i: usize = 0;
+    while i < (stream.len() - size) {
+        let slice = &stream[i..i + size];
+        let set: HashSet<char> = HashSet::from_iter(slice.chars());
+        if set.len() == size {
+            break;
+        }
+        i += 1;
+    }
+    i + size
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::days::day06::find_marker;
+
+
+    #[test]
+    fn part_a_test() {
+        let input = String::from("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+
+        assert_eq!(7, find_marker(input, 4));
+    }
+
+    #[test]
+    fn part_b_test() {
+        let input = String::from("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+
+        assert_eq!(19, find_marker(input, 14));
+    }
+}
--- 2022/solution/rust/src/days/mod.rs
+++ 2022/solution/rust/src/days/mod.rs
@@ -5,11 +5,13 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
-pub const ALL: [&dyn Solution; 5] = [
+pub const ALL: [&dyn Solution; 6] = [
     &day01::Day01,
     &day02::Day02,
     &day03::Day03,
     &day04::Day04,
     &day05::Day05,
+    &day06::Day06,
 ];
